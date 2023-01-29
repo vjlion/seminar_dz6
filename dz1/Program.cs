baf5d26c3205f4c19e1db2c0065f3aa9aed804e8
vjlion <89218741998@mail.ru>
2023-01-29 14:21:45 +0300

    dz6

---
+++ dz1/Program.cs
@@ -0,0 +1,38 @@
+﻿/*
+Задача 41: Пользователь вводит с клавиатуры M чисел. П
+осчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3
+*/
+int getUserDate(string message)
+{
+    Console.ForegroundColor = ConsoleColor.DarkCyan;
+    Console.WriteLine(message);
+    Console.ResetColor();
+    int UserData = int.Parse(Console.ReadLine()!);
+    return UserData;
+}
+int getNumbersKey(int numbers)
+{
+    for (int i = 0; i < numbers; i++)
+    {
+        Console.ForegroundColor = ConsoleColor.DarkCyan;
+        Console.Write($"Введите {i + 1} число: ");
+        Console.ResetColor();
+        int num = int.Parse(Console.ReadLine()!);
+    }
+    return numbers;
+}
+int getUserNum(int array)
+{
+    int count = 0;
+    for (int i = 0; i < array; i++)
+    {
+        if (array > 0) count += 1;
+    }
+    return array;
+}
+int numbers = getUserDate("Введите сколько будет чисел: ");
+int res = getNumbersKey(numbers);
+int user = getUserNum(res);
+Console.WriteLine($"Введено чисел больше '0':  {user}");
---
+++ dz2/Program.cs
@@ -0,0 +1,36 @@
+﻿/*
+Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+значения b1, k1, b2 и k2 задаются пользователем.
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+*/
+
+int getUserDate(string message)
+{
+    Console.ForegroundColor = ConsoleColor.DarkCyan;
+    Console.WriteLine(message);
+    Console.ResetColor();
+    int UserData = int.Parse(Console.ReadLine()!);
+    return UserData;
+}
+void CrossPoint(double k1, double b1, double k2, double b2)
+{
+    double x = (b1 - b2) / (k2 - k1);
+    double y = (k2 * b1 - k1 * b2) / (k2 - k1);
+    if (k1 == k2)
+    {
+        Console.Write("Заданные прямые не пересекаются!");
+    }
+    else
+    {
+        Console.Write($"Точка пересечения заданных прямых: ({x:0.00}; {y:0.00})");
+    }
+}
+
+double b1 = getUserDate("Введите b1: ");
+double k1 = getUserDate("Введите k1: ");
+double b2 = getUserDate("Введите b2: ");
+double k2 = getUserDate("Введите k2: ");
+CrossPoint(k1, b1, k2, b2);
+
+
